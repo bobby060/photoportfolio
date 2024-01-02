2946648b7b877f9892437aada48dfd923a50c6c8
bobby060 <scoutrpn@gmail.com>
2024-01-02 17:23:45 +0900

    no changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "es2021": true
-//     },
-//     "extends": [
-//         "eslint:recommended",
-//         "plugin:react/recommended"
-//     ],
-//     "overrides": [
-//         {
-//             "env": {
-//                 "node": true
-//             },
-//             "files": [
-//                 ".eslintrc.{js,cjs}"
-//             ],
-//             "parserOptions": {
-//                 "sourceType": "script"
-//             }
-//         }
-//     ],
-//     "parserOptions": {
-//         "ecmaVersion": "latest",
-//         "sourceType": "module"
-//     },
-//     "plugins": [
-//         "react"
-//     ],
-//     "rules": {
-//     },
-//     "ignorePatterns": ["amplify-codegen-temp/models/models", "src/models/models", ]
-// }
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "ignorePatterns": ["amplify-codegen-temp/models/models", "src/models/models",]
+}
