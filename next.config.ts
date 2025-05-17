5a1d95b521c44e43346507057f9ed9b373a1b141
bobby060 <scoutrpn@gmail.com>
2025-05-17 15:06:13 -0400

    fix: static images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     trailingSlash: true, // Ensures trailing slashes for all routes
     images: {
         domains: ['d3fxm8v2c5j7cl.cloudfront.net'], // Need to make this an amplify secret or something
+        unoptimized: true, // This will prevent the 404 errors for static images
     },
     // If you're not deploying to root domain, uncomment and set your base path
     // basePath: '/your-base-path',
