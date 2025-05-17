845d6390cd415688beb33bbec10d043f935a7b82
bobby060 <scoutrpn@gmail.com>
2025-05-17 14:16:25 -0400

    update path again

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     // output: 'export', // Outputs a Single-Page Application (SPA)
-    distDir: '_next', // Default Next.js build output directory
+    distDir: '.next', // Default Next.js build output directory
     images: {
         domains: ['d3fxm8v2c5j7cl.cloudfront.net'] // Need to make this an amplify secret or something
     }
