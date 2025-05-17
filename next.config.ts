0015f4c4715726905b1afd15853899d739f16052
bobby060 <scoutrpn@gmail.com>
2025-05-17 15:02:17 -0400

    add trailing slash

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    // output: 'export', // Outputs a Single-Page Application (SPA)
     distDir: '.next', // Default Next.js build output directory
+    trailingSlash: true, // Ensures trailing slashes for all routes
     images: {
-        domains: ['d3fxm8v2c5j7cl.cloudfront.net'] // Need to make this an amplify secret or something
-    }
+        domains: ['d3fxm8v2c5j7cl.cloudfront.net'], // Need to make this an amplify secret or something
+    },
+    // If you're not deploying to root domain, uncomment and set your base path
+    // basePath: '/your-base-path',
 }
 
 export default nextConfig
